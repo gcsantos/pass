0854c1291f8f34841d3dbaeb26093570224559d8
Guilherme Cintra <guilhermecs02@hotmail.com>
2024-04-03 00:48:54 -0300

    page register

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,8 +1,25 @@
-import { Slot } from 'expo-router'
-import '@/styles/global.css'
+import "@/styles/global.css";
 
-export default function Layout(){
-  return (
-  <Slot />
-  )
+import { Slot } from "expo-router";
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@/components/loading";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return <Slot />;
 }
--- mobile/src/app/register.tsx
+++ mobile/src/app/register.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
 import { colors } from "@/styles/colors";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import { View, Image, StatusBar } from "react-native";
-export default function Home() {
+export default function Register() {
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -16,20 +16,29 @@ export default function Home() {
       />
       <View className="w-full mt-12 gap-4">
         <Input>
-          <MaterialCommunityIcons
-            name="ticket-confirmation-outline"
+          <FontAwesome6
+            name="user-circle"
             size={20}
             color={colors.green[200]}
           />
-          <Input.Field placeholder="Código do ingresso" />
+          <Input.Field placeholder="Nome completo" />
         </Input>
 
-        <Button title="Acessar ingresso" />
+        <Input>
+          <MaterialIcons
+            name="alternate-email"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+        </Input>
+
+        <Button title="Gerar ingresso" />
 
         <Link
-          href="/register"
+          href="/"
           className="text-green-100 text-base font-bold text-center mt-8">
-          Ainda não possuis um ingresso?
+          Já possui ingresso?
         </Link>
       </View>
     </View>
